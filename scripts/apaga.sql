27e6267a461007aae863ec2d8961a013344a37d2
Ícaro Pires <icaropsa@gmail.com>
2019-10-29 01:08:41 -0300

    Add ddl scripts for creating and dropping the tables

---
+++ scripts/apaga.sql
@@ -0,0 +1,6 @@
+USE tf_icaro_sara;
+
+drop table OCORRENCIA;
+drop table BOLETIM;
+drop table DELEGACIA;
+drop table DEPARTAMENTO;
---
+++ scripts/fisico.sql
@@ -0,0 +1,58 @@
+CREATE DATABASE tf_icaro_sara;
+USE tf_icaro_sara;
+
+CREATE TABLE DEPARTAMENTO (
+    nomeDepartamento VARCHAR(70) NOT NULL,
+
+    CONSTRAINT DEPARTAMENTO_PK PRIMARY KEY(nomeDepartamento)
+) Engine = InnoDB;
+
+CREATE TABLE DELEGACIA (
+    idDelegacia INTEGER NOT NULL AUTO_INCREMENT,
+    nome VARCHAR(70) NOT NULL,
+    cidade VARCHAR(50),
+
+    nomeDepartamento VARCHAR(70) NOT NULL,
+
+    CONSTRAINT DELEGACIA_PK PRIMARY KEY(idDelegacia),
+    CONSTRAINT DELEGACIA_OCORRENCIA_PK FOREIGN KEY(nomeDepartamento)
+        REFERENCES DEPARTAMENTO(nomeDepartamento)
+            ON DELETE RESTRICT
+            ON UPDATE RESTRICT
+) Engine = InnoDB;
+
+CREATE TABLE BOLETIM (
+    numBO INTEGER NOT NULL AUTO_INCREMENT,
+    anoBO INT NOT NULL,
+    rubrica VARCHAR(50),
+
+    idDelegacia INTEGER NOT NULL,
+    idOcorrencia INTEGER NOT NULL,
+
+    CONSTRAINT BOLETIM_PK PRIMARY KEY(numBO, anoBO),
+    CONSTRAINT BOLETIM_DELEGACIA_FK FOREIGN KEY(idDelegacia)
+        REFERENCES DELEGACIA(idDelegacia)
+            ON DELETE RESTRICT
+            ON UPDATE RESTRICT
+) Engine = InnoDB;
+
+CREATE TABLE OCORRENCIA (
+    idOcorrencia INTEGER NOT NULL AUTO_INCREMENT,
+    data DATE NOT NULL,
+    desdobramento VARCHAR(500),
+    status ENUM('C', 'T') NOT NULL,
+    conduta VARCHAR(50),
+    numero INT NOT NULL,
+    logradouro VARCHAR(50) NOT NULL,
+    cidade VARCHAR(50) NOT NULL,
+    latitude FLOAT,
+    longitute FLOAT,
+
+    numBO INTEGER NOT NULL,
+
+    CONSTRAINT OCORRENCIA_PK PRIMARY KEY(idOcorrencia),
+    CONSTRAINT OCORRENCIA_BOLETIM_FK FOREIGN KEY(numBO)
+        REFERENCES BOLETIM(numBO)
+            ON DELETE RESTRICT
+            ON UPDATE RESTRICT
+) Engine = InnoDB;
